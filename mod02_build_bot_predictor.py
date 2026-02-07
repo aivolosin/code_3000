169ccd0601713ad6f0609ab2db3fcbc7b8a7683a
Aiden <aivolosin@gmail.com>
2026-02-07 17:25:33 -0500

    HW 2 Code

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -10,9 +10,9 @@ def train_model(X, y, seed=seed):
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.1,
-        n_estimators=100,
-        max_depth=8,
+        learning_rate=.018,
+        n_estimators=60,
+        max_depth=5,
         subsample=1,
         min_samples_leaf=1,
         random_state=seed
